c04cae73b86e2894ed426be52acd55696b1edd06
qzh <2806894439@qq.com>
2023-11-06 22:22:29 +0800

    初始开发环境搭建

---
+++ paicoding-web/src/main/java/com/paicoding/PaicodingForumApplication.java
@@ -0,0 +1,11 @@
+package com.paicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaicodingForumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaicodingForumApplication.class, args);
+    }
+}
