230e3267b6845014eef042985993a44a836a7f1c
qzh <2806894439@qq.com>
2023-11-08 22:42:58 +0800

    首页

---
+++ paicoding-web/src/main/java/com/paicoding/web/front/home/vo/IndexVO.java
@@ -0,0 +1,38 @@
+package com.paicoding.web.front.home.vo;
+
+import com.paicoding.api.model.vo.PageListVO;
+import com.paicoding.api.model.vo.article.dto.ArticleDTO;
+import com.paicoding.api.model.vo.article.dto.CategoryDTO;
+import com.paicoding.api.model.vo.recommend.CarouselDTO;
+import com.paicoding.api.model.vo.recommend.SideBarDTO;
+import com.paicoding.api.model.vo.user.dto.UserStatisticInfoDTO;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class IndexVO {
+    //分类列表
+    private List<CategoryDTO> categories;
+
+    //当前选中的分类
+    private String currentCategory;
+
+    //当前选中的类目id
+    private Long categoryId;
+
+    //top文章列表
+    private List<ArticleDTO> topArticles;
+
+    //文章列表
+    private PageListVO<ArticleDTO> articles;
+
+    //登录用户信息
+    private UserStatisticInfoDTO user;
+
+    //侧边栏信息
+    private List<SideBarDTO> sideBarItems;
+
+    //轮播图
+    private List<CarouselDTO> homeCarouselList;
+}
