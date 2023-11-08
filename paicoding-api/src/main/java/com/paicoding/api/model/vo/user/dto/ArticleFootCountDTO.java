bb5af0901588dbda663ef9c88acd9b859df06eaa
qzh <2806894439@qq.com>
2023-11-08 22:27:54 +0800

    文章足迹计数

---
+++ paicoding-api/src/main/java/com/paicoding/api/model/vo/user/dto/ArticleFootCountDTO.java
@@ -0,0 +1,28 @@
+package com.paicoding.api.model.vo.user.dto;
+
+import lombok.Data;
+
+/**
+ * 文章足迹计数
+ */
+@Data
+public class ArticleFootCountDTO {
+    //文章点赞数
+    private Integer praiseCount;
+
+    //文章被阅读数
+    private Integer readCount;
+
+    //文章被收藏数
+    private Integer collectionCount;
+
+    //评论数
+    private Integer commentCount;
+
+    public ArticleFootCountDTO() {
+        praiseCount = 0;
+        readCount = 0;
+        collectionCount = 0;
+        commentCount = 0;
+    }
+}
