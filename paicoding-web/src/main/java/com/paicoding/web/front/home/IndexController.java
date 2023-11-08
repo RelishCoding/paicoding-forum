d17039af1e4d62e3c8b1ff251d9f375657320b84
qzh <2806894439@qq.com>
2023-11-08 22:44:33 +0800

    首页controller

---
+++ paicoding-web/src/main/java/com/paicoding/web/front/home/IndexController.java
@@ -0,0 +1,24 @@
+package com.paicoding.web.front.home;
+
+import com.paicoding.web.front.home.helper.IndexRecommendHelper;
+import com.paicoding.web.front.home.vo.IndexVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+    @Autowired
+    private IndexRecommendHelper indexRecommendHelper;
+
+    @GetMapping(path = {"/", "", "/index", "login"})
+    public String index(Model model, HttpServletRequest request) {
+        String activeTab = request.getParameter("category");
+        IndexVO vo = indexRecommendHelper.buildIndexVO(activeTab);
+        model.addAttribute("vo",vo);
+        return "views/home/index";
+    }
+}
