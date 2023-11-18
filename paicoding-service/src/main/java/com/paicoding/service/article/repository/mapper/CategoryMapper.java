da2344e91380a107c8ad69952f2cdd48013683a7
qzh <2806894439@qq.com>
2023-11-18 22:30:56 +0800

    类目管理mapper接口

---
+++ paicoding-service/src/main/java/com/paicoding/service/article/repository/mapper/CategoryMapper.java
@@ -0,0 +1,10 @@
+package com.paicoding.service.article.repository.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.paicoding.service.article.repository.entity.CategoryDO;
+
+/**
+ * 类目管理mapper接口
+ */
+public interface CategoryMapper extends BaseMapper<CategoryDO> {
+}
