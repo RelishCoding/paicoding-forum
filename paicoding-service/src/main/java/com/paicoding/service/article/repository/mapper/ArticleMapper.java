d525fa96a80be59c019a85ff51cb19a489771a34
qzh <2806894439@qq.com>
2023-11-19 10:29:32 +0800

    文章mapper接口

---
+++ paicoding-service/src/main/java/com/paicoding/service/article/repository/mapper/ArticleMapper.java
@@ -0,0 +1,10 @@
+package com.paicoding.service.article.repository.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.paicoding.service.article.repository.entity.ArticleDO;
+
+/**
+ * 文章mapper接口
+ */
+public interface ArticleMapper extends BaseMapper<ArticleDO> {
+}
