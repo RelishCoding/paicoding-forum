1896076bcbf8c85c1ab894d45c22b55189f50e95
qzh <2806894439@qq.com>
2023-11-19 11:48:07 +0800

    继承BaseViewController

--- paicoding-web/src/main/java/com/paicoding/web/front/home/IndexController.java
+++ paicoding-web/src/main/java/com/paicoding/web/front/home/IndexController.java
@@ -2,6 +2,7 @@ package com.paicoding.web.front.home;
 
 import com.paicoding.web.front.home.helper.IndexRecommendHelper;
 import com.paicoding.web.front.home.vo.IndexVO;
+import com.paicoding.web.global.BaseViewController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class IndexController {
+public class IndexController extends BaseViewController {
     @Autowired
     private IndexRecommendHelper indexRecommendHelper;
 
