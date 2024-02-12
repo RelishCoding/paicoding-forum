cfb89ac5c1df4f40fc35988a599a7af82dd2bf42
qzh <2806894439@qq.com>
2024-02-12 22:44:39 +0800

    Permission注解

---
+++ paicoding-core/src/main/java/com/paicoding/core/permission/Permission.java
@@ -0,0 +1,14 @@
+package com.paicoding.core.permission;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Permission {
+    /**
+     * 限定权限
+     * @return
+     */
+    UserRole role() default UserRole.ALL;
+}
