a1daf6a73a4ffb2143fd8af48ce2806b9ca79018
qzh <2806894439@qq.com>
2024-02-13 10:46:05 +0800

    配置mapper接口

---
+++ paicoding-service/src/main/java/com/paicoding/service/config/repository/mapper/ConfigMapper.java
@@ -0,0 +1,10 @@
+package com.paicoding.service.config.repository.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.paicoding.service.config.repository.entity.ConfigDO;
+
+/**
+ * 配置mapper接口
+ */
+public interface ConfigMapper extends BaseMapper<ConfigDO> {
+}
