895ae69b6f86abbbd12eec64c2e5cc83c307448b
qzh <2806894439@qq.com>
2024-02-13 17:40:22 +0800

    GlobalVo

---
+++ paicoding-web/src/main/java/com/paicoding/web/global/vo/GlobalVo.java
@@ -0,0 +1,42 @@
+package com.paicoding.web.global.vo;
+
+import com.paicoding.api.model.vo.seo.SeoTagVo;
+import com.paicoding.api.model.vo.user.dto.BaseUserInfoDTO;
+import com.paicoding.service.sitemap.model.SiteCntVo;
+import com.paicoding.web.config.GlobalViewConfig;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GlobalVo {
+    //网站相关配置
+    private GlobalViewConfig siteInfo;
+
+    //站点统计信息
+    private SiteCntVo siteStatisticInfo;
+
+    //今日的站点统计信息
+    private SiteCntVo todaySiteStatisticInfo;
+
+    //环境
+    private String env;
+
+    //是否已登录
+    private Boolean isLogin;
+
+    //登录用户信息
+    private BaseUserInfoDTO user;
+
+    //消息通知数量
+    private Integer msgNum;
+
+    //在线用户人数
+    private Integer onlineCnt;
+
+    private String currentDomain;
+
+    private List<SeoTagVo> ogp;
+
+    private String jsonLd;
+}
