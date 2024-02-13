e8728d658235a511204f65fcbee8cd23ca00ee57
qzh <2806894439@qq.com>
2024-02-13 15:41:45 +0800

    ai用户表

---
+++ paicoding-service/src/main/java/com/paicoding/service/user/repository/entity/UserAiDO.java
@@ -0,0 +1,52 @@
+package com.paicoding.service.user.repository.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.paicoding.api.model.entity.BaseDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * ai用户表
+ * @ClassName: UserAiDO
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("user_ai")
+public class UserAiDO extends BaseDO {
+    // 用户id
+    private Long userId;
+
+    // 知识星球编号
+    private String starNumber;
+
+    //星球来源 1=java进阶之路 2=技术派
+    private Integer starType;
+
+    // 当前用户绑定的邀请者
+    private Long inviterUserId;
+
+    // 邀请码
+    private String inviteCode;
+
+    // 当前用户邀请的人数
+    private Integer inviteNum;
+
+    /**
+     * 二进制使用姿势
+     * 第0位： = 1 表示已绑定微信公众号
+     * 第1位： = 1 表示绑定了邀请用户
+     * 第2位： = 1 表示绑定了java星球
+     * 第3位： = 1 表示绑定了技术派星球
+     */
+    private Integer strategy;
+
+    /**
+     * 0 审核中 1 试用中 2 审核通过 3 审核拒绝
+     */
+    private Integer state;
+
+    // 是否删除
+    private Integer deleted;
+}
