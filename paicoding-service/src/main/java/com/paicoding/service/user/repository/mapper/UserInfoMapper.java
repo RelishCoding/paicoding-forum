32a9e9ecc0642a3dc2a5f5b9eb13543fb6f98401
qzh <2806894439@qq.com>
2024-02-13 15:50:23 +0800

    用户个人信息mapper接口

---
+++ paicoding-service/src/main/java/com/paicoding/service/user/repository/mapper/UserInfoMapper.java
@@ -0,0 +1,10 @@
+package com.paicoding.service.user.repository.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.paicoding.service.user.repository.entity.UserInfoDO;
+
+/**
+ * 用户个人信息mapper接口
+ */
+public interface UserInfoMapper extends BaseMapper<UserInfoDO> {
+}
