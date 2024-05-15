52b6f202bc5263d726d5b61c52e2426e7dcc48b6
qzh <2806894439@qq.com>
2024-05-15 21:48:38 +0800

    添加方法

--- paicoding-web/src/main/java/com/paicoding/web/global/BaseViewController.java
+++ paicoding-web/src/main/java/com/paicoding/web/global/BaseViewController.java
@@ -1,7 +1,34 @@
 package com.paicoding.web.global;
 
+import com.paicoding.api.model.vo.PageParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 /**
  * 全局属性配置
  */
 public class BaseViewController {
+    @Autowired
+    protected GlobalInitService globalInitService;
+
+    public PageParam buildPageParam(Long page, Long size) {
+        if (page <= 0) {
+            page = PageParam.DEFAULT_PAGE_NUM;
+        }
+        if (size == null || size > PageParam.DEFAULT_PAGE_SIZE) {
+            size = PageParam.DEFAULT_PAGE_SIZE;
+        }
+        return PageParam.newPageInstance(page, size);
+    }
+
+    // 推荐使用它替代 GlobalViewInterceptor 中的全局属性设置
+    /**
+     * 全局属性配置
+     * @param model
+     */
+    /*@ModelAttribute
+    public void globalAttr(Model model) {
+        model.addAttribute("global", globalInitService.globalAttr());
+    }*/
 }
